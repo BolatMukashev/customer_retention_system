3599f3a8c299262b4bacdca400bf7bdf55b333f0
BolatMukashev <ya.ne.angel.kimi@gmail.com>
2026-07-21 15:12:37 +0500

    добавление и изменение клиента

--- crs/clients/views.py
+++ crs/clients/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Client
+from .forms import ClientForm
 from events.models import Event
 from orders.models import Order
 
@@ -21,9 +22,37 @@ def view(request, pk):
     return render(request, 'clients/view.html', {'client': client, "events": events, "orders": orders, "org": org})
 
 
+@login_required
 def add(request):
-    pass
+    if request.method == "POST":
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.organization = request.user.organization
+            client.save()
+            return redirect("clients:index")
+    else:
+        form = ClientForm()
+
+    return render(request, "clients/add.html", {
+        "form": form,
+    })
 
 
-def edit(request):
-    pass
+@login_required
+def edit(request, pk):
+    org = request.user.organization
+    client = get_object_or_404(Client, pk=pk, organization=org)
+
+    if request.method == "POST":
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect("clients:client_view", pk=client.pk)
+    else:
+        form = ClientForm(instance=client)
+
+    return render(request, "clients/edit.html", {
+        "form": form,
+        "client": client,
+    })
